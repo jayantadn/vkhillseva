e92046abfeab9a9c94dde2a7ac350cc2b8630548
JD1411 <jyd1kor@bosch.com>
2024-12-25 13:26:52 +0530

    update notification handling and version to 0.6.1; add Firebase messaging setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:vkhillseva/common/notification.dart';
 import 'package:vkhillseva/common/theme.dart';
 import 'package:vkhillseva/common/toaster.dart';
 import 'package:vkhillseva/home/home.dart';
@@ -11,24 +12,6 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   print('Background message received: ${message.messageId}');
 }
 
-void _requestPermission() async {
-  FirebaseMessaging messaging = FirebaseMessaging.instance;
-
-  NotificationSettings settings = await messaging.requestPermission(
-    alert: true,
-    badge: true,
-    sound: true,
-  );
-
-  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
-    // nothing to do
-  } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
-    Toaster().error('User granted provisional permission');
-  } else {
-    Toaster().error('User declined or has not accepted permission');
-  }
-}
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -36,7 +19,6 @@ Future<void> main() async {
   );
 
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  _requestPermission();
 
   runApp(MyApp());
 }
@@ -45,7 +27,7 @@ class MyApp extends StatelessWidget {
   MyApp({super.key});
 
   final Widget home = const HomePage(title: "Hare Krishna");
-  final Widget test = NityaSeva(title: "testing");
+  final Widget test = NotificationHomePage();
   // final Widget test = TicketPage(
   //     session: Session(
   //         name: "Morning Nitya Seva",

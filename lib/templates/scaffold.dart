3b2567ddd6370d905bf867e1de11133f41b786de
JD1411 <jyd1kor@bosch.com>
2024-12-29 20:25:28 +0530

    android apk icon

--- lib/templates/scaffold.dart
+++ lib/templates/scaffold.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:synchronized/synchronized.dart';
 import 'package:vkhillseva/widgets/common_widgets.dart';

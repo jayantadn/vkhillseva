4f31bdf6431a17a014a1748f8d995455424f2b90
JD1411 <jyd1kor@bosch.com>
2024-12-29 13:34:19 +0530

    release 0.13.0

--- lib/common/const.dart
+++ lib/common/const.dart
@@ -12,7 +12,7 @@ class Const {
   }
 
   final String dbroot = "VKHILLSEVA_01";
-  final String version = "0.12.0";
+  final String version = "0.13.0";
 
   final int fbListenerDelay = 2; // seconds
 

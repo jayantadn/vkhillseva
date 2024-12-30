d452000070cd7261d6f4b2401d1b5fe9260dbc5e
JD1411 <jyd1kor@bosch.com>
2024-12-30 17:08:32 +0530

    update version to 0.14.4 and change home widget to test

--- lib/common/const.dart
+++ lib/common/const.dart
@@ -12,7 +12,7 @@ class Const {
   }
 
   final String dbroot = "VKHILLSEVA_01";
-  final String version = "0.14.3";
+  final String version = "0.14.4";
 
   final int fbListenerDelay = 2; // seconds
 

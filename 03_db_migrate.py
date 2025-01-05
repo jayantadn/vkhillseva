7062cf93db9748241738f9f4433e0087e537712c
JD1411 <jyd1kor@bosch.com>
2025-01-05 05:41:08 +0530

    release 0.18.11

--- 03_db_migrate.py
+++ 03_db_migrate.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 
 input_json = r"X:\GoogleDrive\PublicRO\Garuda\database_backup\input.json"
@@ -16,7 +17,37 @@ def migrate_sessions():
         output_data = json.load(file)
 
     for session in input_data['record_db1']['sevaTickets']:
-        print (session)
+        for ticketKey in input_data['record_db1']['sevaTickets'][session]:
+            ticket = input_data['record_db1']['sevaTickets'][session][ticketKey]
+            db_date = ticket['timestampSlot'][:10]
+            db_session = ticket['timestampSlot'].replace('.', '^')
+            key = ticket['timestampTicket'].replace('.', '^')
+            if ticket['amount'] == 400:
+                image = "assets/images/LauncherIcons/NityaSeva.png"
+                seva = "Pushpanjali"
+            elif ticket['amount'] == 500:
+                image = "assets/images/NityaSeva/tas.png"
+                seva = "Tulasi Archana Seva"
+            elif ticket['amount'] == 1000:
+                image = "assets/images/Logo/KrishnaLilaPark_square.png"
+                seva = "Mandir-marjana Seva"
+            elif ticket['amount'] == 2500:
+                image = "assets/images/NityaSeva/gita.png"
+                seva = "Gita-Dana Seva"
+            else:
+                print("Unknown seva amount: ", ticket['amount'])
+                sys.exit(1)
+            t = {
+                "amount": ticket['amount'],
+                "image": image,
+                "mode": ticket['mode'],
+                "note": "",
+                "seva": seva,
+                "ticketNumber": ticket['ticket'],
+                "timestamp": ticket['timestampTicket'],
+                "user": ticket['user']
+            }
+            output_data[db_date][db_session]['Tickets'][key] = t
     
     print("Writing output file...")
     with open(output_json, 'w') as file:

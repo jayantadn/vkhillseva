b63be6aab1af44d70c425837f97b3c39f35a2e81
JD1411 <jyd1kor@bosch.com>
2025-01-09 20:23:15 +0530

    release 0.20.0

--- lib/common/const.dart
+++ lib/common/const.dart
@@ -12,7 +12,7 @@ class Const {
   }
 
   final String dbroot = "VKHILLSEVA_01";
-  final String version = "1.2.0";
+  final String version = "0.20.0";
 
   final int fbListenerDelay = 2; // seconds
   final double toolbarIconSize = 32;

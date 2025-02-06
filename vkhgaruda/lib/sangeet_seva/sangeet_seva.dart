690368983e8d4f80632eaf1155cd331d44aa5e47
Jayanta <Jayanta.Debnath@in.bosch.com>
2025-02-06 15:59:44 +0530

    refactor calendar functionality by creating a separate Calendar widget and simplifying the SangeetSeva state management

--- vkhgaruda/lib/sangeet_seva/sangeet_seva.dart
+++ vkhgaruda/lib/sangeet_seva/sangeet_seva.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:synchronized/synchronized.dart';
+import 'package:vkhgaruda/sangeet_seva/calendar.dart';
 import 'package:vkhgaruda/sangeet_seva/profiles.dart';
 import 'package:vkhgaruda/widgets/loading_overlay.dart';
 import 'package:vkhgaruda/common/theme.dart';
@@ -22,7 +23,7 @@ class _SangeetSevaState extends State<SangeetSeva> {
   // scalars
   final Lock _lock = Lock();
   bool _isLoading = true;
-  DateTime _selectedDay = DateTime.now();
+  final Widget _calendar = Calendar();
 
   // lists
   List<int> _numBookings = [];
@@ -69,84 +70,6 @@ class _SangeetSevaState extends State<SangeetSeva> {
     });
   }
 
-  Widget _createCalendarDay(
-      {required DateTime day,
-      bool? border,
-      bool? fill,
-      int? greenstars,
-      int? redstars}) {
-    return Padding(
-      padding: const EdgeInsets.all(2.0),
-      child: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            color: (fill != null && fill == true)
-                ? Colors.blue[50]
-                : Colors.transparent,
-            border: border == true ? Border.all(color: Colors.grey) : null,
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                '${day.day}',
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    redstars != null ? greenstars.toString() : ' ',
-                    style: TextStyle(color: Colors.red),
-                  ),
-                  Text(greenstars != null ? greenstars.toString() : ' ',
-                      style: TextStyle(color: Colors.green)),
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _createCalendarView(BuildContext context) {
-    DateTime now = DateTime.now();
-
-    return TableCalendar(
-      firstDay: DateTime(2024),
-      lastDay: DateTime.now().add(Duration(days: 90)),
-      focusedDay: DateTime.now(),
-      selectedDayPredicate: (day) {
-        return isSameDay(_selectedDay, day);
-      },
-      calendarBuilders: CalendarBuilders(
-        defaultBuilder: (context, day, focusedDay) {
-          return _createCalendarDay(day: day);
-        },
-        todayBuilder: (context, day, focusedDay) {
-          return _createCalendarDay(day: day, border: true);
-        },
-        selectedBuilder: (context, day, focusedDay) {
-          return _createCalendarDay(
-              day: day,
-              border: now.day == day.day &&
-                  now.month == day.month &&
-                  now.year == day.year,
-              fill: true);
-        },
-      ),
-      onDaySelected: (selectedDay, focusedDay) {
-        setState(() {
-          _selectedDay = selectedDay;
-        });
-      },
-      availableCalendarFormats: const {
-        CalendarFormat.month: 'Month',
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Theme(
@@ -181,11 +104,18 @@ class _SangeetSevaState extends State<SangeetSeva> {
                           SizedBox(height: 10),
 
                           // your widgets here
-                          _createCalendarView(context),
+                          _calendar,
 
                           // leave some space at bottom
                           SizedBox(height: 100),
                         ])))),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                // Add your onPressed code here!
+              },
+              tooltip: 'Add',
+              child: Icon(Icons.add),
+            ),
           ),
 
           // circular progress indicator

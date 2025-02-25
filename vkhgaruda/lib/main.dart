25377a1431b9f9384f87f25982cb33e2bba4a4d0
JD1411 <jyd1kor@bosch.com>
2025-02-25 09:24:32 +0530

    update app titles and add version file

--- vkhgaruda/lib/main.dart
+++ vkhgaruda/lib/main.dart
@@ -22,6 +22,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "Garuda", theme: themeDefault, home: home);
+    return MaterialApp(
+      title: 'Garuda',
+      theme: themeDefault,
+      home: home,
+    );
   }
 }
---
+++ vkhgaruda/lib/version.dart
@@ -0,0 +1,1 @@
+final String version = "3.1.0";

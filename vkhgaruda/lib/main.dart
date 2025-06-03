a351e6bbe6980467ae14768c4f132517a1f239b0
Jayanta <jayanta.dn@gmail.com>
2025-06-03 21:37:31 +0530

    release 7.7.0

--- vkhgaruda/lib/main.dart
+++ vkhgaruda/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:vkhgaruda/home/landing.dart';
-import 'package:vkhgaruda/nitya_seva/festival_by_event.dart';
 import 'firebase_options.dart';
 import 'package:vkhpackages/vkhpackages.dart';
 

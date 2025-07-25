e576b6353c5f3ca6a841ac91e0b6a14b3777caa9
Jayanta <jayanta.dn@gmail.com>
2025-07-25 21:12:47 +0530

    removed sangeet seva

--- vkhsangeetseva/android/app/src/main/kotlin/com/example/vkhsangeetseva/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.vkhsangeetseva
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- vkhsangeetseva/test/sangeetseva.md
+++
@@ -1,7 +0,0 @@
-# General
-- signup
-
-# Sangeet Seva
-- set up profile
-- request for event
-- view requested event

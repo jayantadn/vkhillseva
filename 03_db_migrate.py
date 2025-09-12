f8848288f24a63f2d6402e88e8179054676c1849
Jayanta Debnath <Jayanta.Dn@gmail.com>
2025-09-12 22:38:54 +0530

    import 2024 deepotsava data

--- 03_db_migrate.py
+++ 03_db_migrate.py
@@ -2,8 +2,8 @@ import json
 import sys
 
 
-input_json = r"X:\GoogleDrive\PublicRO\Garuda\database_backup\input.json"
-output_json = r"X:\GoogleDrive\PublicRO\Garuda\database_backup\output.json"
+input_json = r"input_deepotsava_2024.json"
+output_json = r"output_deepotsava_2024.json"
 template_json = r"X:\GoogleDrive\PublicRO\Garuda\database_backup\template-NityaSeva.json"
 
 def migrate_sessions():
@@ -55,8 +55,61 @@ def migrate_sessions():
 
     print("Data migration completed.")
 
+def import_deepotsava():
+    output_data = {
+        'RKC' : {
+            'Sales': {}
+        },
+        'RRG' : {
+            'Sales': {}
+        }
+    }
+
+    with open(input_json, 'r') as file:
+        input_data = json.load(file)
+
+        print("processing RKC sales")
+        rkc_sales = input_data['record_db1']['deepotsava']['RKC']['sales']
+        for sale in rkc_sales:
+            dbdate = rkc_sales[sale]['timestamp'][:10]
+            dbtime = rkc_sales[sale]['timestamp'].replace('.', '^')
+            if dbdate not in output_data['RKC']['Sales']:
+                output_data['RKC']['Sales'][dbdate] = {}
+            output_data['RKC']['Sales'][dbdate][dbtime] = {
+                'count' : rkc_sales[sale]['count'],
+                'deepamPrice': rkc_sales[sale]['costLamp'],
+                'isPlateIncluded': False if rkc_sales[sale]['plate'] == 0 else True,
+                'paymentMode': rkc_sales[sale]['paymentMode'],
+                'platePrice': rkc_sales[sale]['costPlate'],
+                'timestamp': rkc_sales[sale]['timestamp'],
+                'username': rkc_sales[sale]['user']
+            }
+            
+        print("processing RRG sales")
+        rkc_sales = input_data['record_db1']['deepotsava']['RRG']['sales']
+        for sale in rkc_sales:
+            dbdate = rkc_sales[sale]['timestamp'][:10]
+            dbtime = rkc_sales[sale]['timestamp'].replace('.', '^')
+            if dbdate not in output_data['RRG']['Sales']:
+                output_data['RRG']['Sales'][dbdate] = {}
+            output_data['RRG']['Sales'][dbdate][dbtime] = {
+                'count' : rkc_sales[sale]['count'],
+                'deepamPrice': rkc_sales[sale]['costLamp'],
+                'isPlateIncluded': False if rkc_sales[sale]['plate'] == 0 else True,
+                'paymentMode': rkc_sales[sale]['paymentMode'],
+                'platePrice': rkc_sales[sale]['costPlate'],
+                'timestamp': rkc_sales[sale]['timestamp'],
+                'username': rkc_sales[sale]['user']
+            }
+
+    with open(output_json, 'w') as file:
+        json.dump(output_data, file, indent=2)
+    print(f"Data written to {output_json}")
+    
+
 def main():
-    migrate_sessions()
+    # migrate_sessions()
+    import_deepotsava()
 
 if __name__ == "__main__":
     main()

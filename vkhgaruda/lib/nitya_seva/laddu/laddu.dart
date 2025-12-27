d1a09a9ffc47d67c68a7a26428dc921470a066f2
Jayanta <jayanta.dn@gmail.com>
2025-12-27 20:39:18 +0530

    enhance session data handling and service entries management

--- vkhgaruda/lib/nitya_seva/laddu/laddu.dart
+++ vkhgaruda/lib/nitya_seva/laddu/laddu.dart
@@ -25,14 +25,17 @@ class _LadduState extends State<LadduMain> {
   final Lock _lock = Lock();
   bool _isLoading = true;
   final Set _loadedKeys = {};
-  Map<String, dynamic> _ladduSessionData = {};
 
   // global keys
   final GlobalKey<SingleBarChartState> _keySingleBarChart =
       GlobalKey<SingleBarChartState>();
   final GlobalKey<KVTableState> _keyKVTable = GlobalKey<KVTableState>();
 
-  // lists
+  // lists and maps
+  List<LadduStock> _stocks = [];
+  List<LadduServe> _serves = [];
+  final Map<String, dynamic> _ladduSessionData = {};
+  final Map<DateTime, Map<String, dynamic>> _serviceEntries = {};
 
   // controllers, listeners and focus nodes
   List<StreamSubscription<DatabaseEvent>> _listeners = [];
@@ -47,6 +50,10 @@ class _LadduState extends State<LadduMain> {
   @override
   dispose() {
     // clear all lists and maps
+    _stocks.clear();
+    _serves.clear();
+    _ladduSessionData.clear();
+    _serviceEntries.clear();
 
     // dispose all controllers and focus nodes
 
@@ -69,11 +76,50 @@ class _LadduState extends State<LadduMain> {
       // your code here
 
       // read database and populate data
-      _ladduSessionData = await FBL().readLatestLadduSessionData() ?? {};
+      _ladduSessionData.clear();
+      _ladduSessionData.addAll(await FBL().readLatestLadduSessionData() ?? {});
+      Map<String, dynamic> stocksMap =
+          Map<String, dynamic>.from(_ladduSessionData['stocks']);
+      _stocks = stocksMap.values
+          .toList()
+          .map((entry) =>
+              Utils().convertRawToDatatype(entry, LadduStock.fromJson))
+          .toList();
+      Map<String, dynamic> servesMap =
+          Map<String, dynamic>.from(_ladduSessionData['serves']);
+      _serves = servesMap.values
+          .toList()
+          .map((entry) =>
+              Utils().convertRawToDatatype(entry, LadduServe.fromJson))
+          .toList();
       int availableLadduPacks = _calculateAvailableLadduPacks();
       int totalLadduPacks = _calculateTotalLadduPacks();
 
-      // refresh all child widgets
+      // service entries
+      _serviceEntries.clear();
+      for (LadduStock stock in _stocks) {
+        _serviceEntries[stock.timestamp] = {"stock": stock};
+      }
+      for (LadduServe serve in _serves) {
+        _serviceEntries[serve.timestamp] = {"serve": serve};
+      }
+      if (_ladduSessionData['returned'] != null) {
+        Map<String, dynamic> returnedMap =
+            Map<String, dynamic>.from(_ladduSessionData['returned']);
+        _serviceEntries[returnedMap['timestamp']] = {"return": returnedMap};
+      }
+
+      // sort service entries by timestamp (descending)
+      List<DateTime> sortedKeys = _serviceEntries.keys.toList()
+        ..sort((a, b) => b.compareTo(a));
+      Map<DateTime, Map<String, dynamic>> sortedServiceEntries = {};
+      for (var key in sortedKeys) {
+        sortedServiceEntries[key] = _serviceEntries[key]!;
+      }
+      _serviceEntries.clear();
+      _serviceEntries.addAll(sortedServiceEntries);
+
+      //-- refresh all child widgets
 
       // availability bar
       _keySingleBarChart.currentState?.updateChart(
@@ -152,29 +198,15 @@ class _LadduState extends State<LadduMain> {
     // stocks - (serves + returned)
 
     // total stocks
-    Map<String, dynamic> stocksMap =
-        Map<String, dynamic>.from(_ladduSessionData['stocks']);
-    List<LadduStock> stocks = stocksMap.values
-        .toList()
-        .map(
-            (entry) => Utils().convertRawToDatatype(entry, LadduStock.fromJson))
-        .toList();
     int stockTotal = 0;
-    for (var stock in stocks) {
+    for (var stock in _stocks) {
       stockTotal += stock.count;
       stockTotal += stock.carry ?? 0;
     }
 
     // total serves
-    Map<String, dynamic> servesMap =
-        Map<String, dynamic>.from(_ladduSessionData['serves']);
-    List<LadduServe> serves = servesMap.values
-        .toList()
-        .map(
-            (entry) => Utils().convertRawToDatatype(entry, LadduServe.fromJson))
-        .toList();
     int serveTotal = 0;
-    for (var serve in serves) {
+    for (var serve in _serves) {
       // misc packs
       List miscList = serve.packsMisc;
       serveTotal += miscList.fold<int>(0, (sum, misc) {
@@ -211,23 +243,78 @@ class _LadduState extends State<LadduMain> {
   int _calculateTotalLadduPacks() {
     if (_ladduSessionData.isEmpty) return 0;
 
-    // stocks
-
     // total stocks
-    Map<String, dynamic> stocksMap =
-        Map<String, dynamic>.from(_ladduSessionData['stocks']);
-    List<LadduStock> stocks = stocksMap.values
-        .toList()
-        .map(
-            (entry) => Utils().convertRawToDatatype(entry, LadduStock.fromJson))
-        .toList();
     int stockTotal = 0;
-    for (var stock in stocks) {
+    for (var stock in _stocks) {
       stockTotal += stock.count;
       stockTotal += stock.carry ?? 0;
     }
 
     return stockTotal;
+  }
+
+  Widget _createServiceEntryTile(DateTime timestamp) {
+    String entryType = _serviceEntries[timestamp]?.keys.first ?? 'Unknown';
+    LadduStock? stock;
+    LadduServe? serve;
+    int serveTotal = 0;
+    int returnTotal = 0;
+    Map<String, dynamic> returned = {};
+    if (entryType == 'stock') {
+      stock = _serviceEntries[timestamp]?.values.first;
+    } else if (entryType == 'serve') {
+      serve = _serviceEntries[timestamp]?.values.first;
+
+      // misc packs
+      List miscList = serve?.packsMisc ?? [];
+      serveTotal += miscList.fold<int>(0, (sum, misc) {
+        Map<String, int> kvMap = Map<String, int>.from(misc);
+        return sum + kvMap.values.fold<int>(0, (s, val) => s + val);
+      });
+
+      // other sevas
+      List otherSevaList = serve?.packsOtherSeva ?? [];
+      serveTotal += otherSevaList.fold<int>(0, (sum, otherSeva) {
+        Map<String, int> kvMap = Map<String, int>.from(otherSeva);
+        return sum + kvMap.values.fold<int>(0, (s, val) => s + val);
+      });
+
+      // pushpanjali
+      List pushpanjaliList = serve?.packsPushpanjali ?? [];
+      serveTotal += pushpanjaliList.fold<int>(0, (sum, pushpanjali) {
+        Map<String, int> kvMap = Map<String, int>.from(pushpanjali);
+        return sum + kvMap.values.fold<int>(0, (s, val) => s + val);
+      });
+    } else if (entryType == "return") {
+      returned = Map<String, dynamic>.from(
+          _serviceEntries[timestamp]?.values.first ?? {});
+      returnTotal = returned['count'] ?? 0;
+    }
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Widgets().createTopLevelCard(
+          context: context,
+          title: entryType == 'stock'
+              ? "Stock + ${stock?.count ?? 0 + (stock?.carry ?? 0)}"
+              : (entryType == 'serve'
+                  ? "Serve - $serveTotal    "
+                  : "Return - $returnTotal"),
+          color: entryType == 'stock'
+              ? Colors.green
+              : (entryType == 'serve' ? Colors.orange : Colors.grey),
+          child: Column(
+            children: [
+              Text(
+                entryType == 'stock'
+                    ? stock?.user ?? "Unknown"
+                    : (entryType == 'serve'
+                        ? serve?.user ?? "Unknown"
+                        : returned['user'] ?? "Unknown"),
+              )
+            ],
+          )),
+    );
   }
 
   void _deleteData(Map data) {
@@ -236,6 +323,8 @@ class _LadduState extends State<LadduMain> {
 
   @override
   Widget build(BuildContext context) {
+    List<DateTime> serviceEntryKeys = _serviceEntries.keys.toList();
+
     return Stack(
       children: [
         ResponsiveScaffold(
@@ -285,6 +374,74 @@ class _LadduState extends State<LadduMain> {
                           child: KVTable(
                             key: _keyKVTable,
                           )),
+
+                      // button row
+                      Divider(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          // stock button
+                          ElevatedButton.icon(
+                            onPressed: () async {
+                              // addEditStock(context);
+                            },
+                            icon: Icon(
+                              Icons.add,
+                              color: Colors.white,
+                            ),
+                            label: Text('Stock'),
+                          ),
+
+                          // serve button
+                          ElevatedButton.icon(
+                            onPressed:
+
+                                // (_lr == null || _lr!.count == -1)
+                                //     ? () async {
+                                //         _createServeDialog(context);
+                                //       }
+                                //     :
+                                null,
+                            icon: Icon(Icons.remove, color: Colors.white),
+                            label: Text('Serve'),
+                          ),
+
+                          // return button
+                          ElevatedButton.icon(
+                            onPressed:
+                                // (_lr == null || _lr!.count == -1)
+                                //     ? () {
+                                //         returnStock(context);
+                                //       }
+                                //     :
+                                null,
+                            icon: Icon(Icons.undo, color: Colors.white),
+                            label: Text('Return'),
+                          )
+                        ],
+                      ),
+                      Divider(),
+
+                      // Service Entries
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Service Entries",
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                      SizedBox(
+                        height: 1000,
+                        child: ListView.builder(
+                            itemCount: serviceEntryKeys.length,
+                            itemBuilder: (context, index) {
+                              return SizedBox(
+                                height: 50,
+                                child: _createServiceEntryTile(
+                                    serviceEntryKeys[index]),
+                              );
+                            }),
+                      ),
 
                       // leave some space at bottom
                       SizedBox(height: 100),
